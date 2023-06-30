9fcc1429ea0275bb3a524b66c643f50aefbbf716
Harikeshav R <r.harikeshav@gmail.com>
2023-06-30 17:47:21 +0530

    Create create events test

---
+++ Testings/create_events.py
@@ -0,0 +1,33 @@
+import requests
+
+API_URL = "http://0.0.0.0:8000/api"
+api_key = input("API Key: ")
+
+events = [
+    "Agon",
+    "Apollo",
+    "Auctus",
+    "Enigmata",
+    "Mask A Raid",
+    "Mercatus",
+    "Nemesis",
+    "Saturn",
+    "Sonneto",
+    "Sum it Up!",
+    "Breaking Barriers",
+    "Cognitoria",
+    "Forum Infinitum",
+    "Hacksquads",
+    "Mathletics",
+    "Mathopoly",
+    "Mystifiezzera",
+    "Operation Breakout",
+    "Picstraction",
+    "Rhythmatics",
+]
+
+
+for event in events:
+    requests.post(
+        f"{API_URL}/event/", params={"api-key": api_key}, json={"event_name": event}
+    )
